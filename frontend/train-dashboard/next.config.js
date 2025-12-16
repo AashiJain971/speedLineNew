f7267c5f12c95b1e5e4ad3b80fd6d514b8850409
Aashi jain <aashij971@gmail.com>
2025-12-16 12:32:02 +0530

    pushing updated build

--- frontend/train-dashboard/next.config.js
+++ frontend/train-dashboard/next.config.js
@@ -1,10 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false, // Disabled to prevent duplicate API calls during development
+//   swcMinify: true,
+//   images: {
+//     domains: ['localhost'],
+//   },
+// }
+
+// module.exports = nextConfig
+
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // Disabled to prevent duplicate API calls during development
+  reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['localhost'],
+    domains: ["localhost"],
   },
-}
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
